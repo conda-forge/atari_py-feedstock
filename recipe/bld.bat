77fdafacb8a631ff1908e11d1f1a1230521fa065
Vasily Litvinov <vasilij.n.litvinov@intel.com>
2020-11-25 15:15:10 +0300

    Windows package passes tests
    Signed-off-by: Vasily Litvinov <vasilij.n.litvinov@intel.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,26 +1,27 @@
-pushd "%SRC_DIR%" || exit /b
+SETLOCAL EnableDelayedExpansion
+
+pushd "%SRC_DIR%" || exit /b !ERRORLEVEL!
 
 rem Inline "installzlib.bat" and modify so it can be executed without MSYS
-curl -O https://zlib.net/zlib1211.zip || exit /b
-mkdir atari_py\ale_interface\src\zlib || exit /b
-unzip zlib1211.zip || exit /b
-xcopy /E zlib-1.2.11 atari_py\ale_interface\src\zlib || exit /b
-pushd atari_py\ale_interface\src\zlib || exit /b 
-cmake -DCMAKE_GENERATOR_PLATFORM=x64 . || exit /b
-cmake --build . || exit /b
+curl -O https://zlib.net/zlib1211.zip || exit /b !ERRORLEVEL!
+mkdir atari_py\ale_interface\src\zlib || exit /b !ERRORLEVEL!
+unzip -o zlib1211.zip || exit /b !ERRORLEVEL!
+xcopy /E zlib-1.2.11 atari_py\ale_interface\src\zlib || exit /b !ERRORLEVEL!
+pushd atari_py\ale_interface\src\zlib || exit /b !ERRORLEVEL!
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release . || exit /b !ERRORLEVEL!
+cmake --build . || exit /b !ERRORLEVEL!
 
-cd ..\.. || exit /b
-mkdir build || exit /b
-cp src\zlib\Debug\zlibstaticd.lib build\z.lib || exit /b
+cd ..\.. || exit /b !ERRORLEVEL!
+mkdir build || exit /b !ERRORLEVEL!
+copy src\zlib\zlibstatic.lib build\z.lib || exit /b !ERRORLEVEL!
 popd
 rem End of inline
 
-mkdir atari_py\ale_interface\build || exit /b
-pushd atari_py\ale_interface\build || exit /b
-cmake -DCMAKE_GENERATOR_PLATFORM=x64 .. || exit /b
-cmake --build . || exit /b
-copy Debug\ale_c.dll .. || exit /b
+pushd atari_py\ale_interface\build || exit /b !ERRORLEVEL!
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release .. || exit /b !ERRORLEVEL!
+cmake --build . || exit /b !ERRORLEVEL!
+copy ale_c.dll .. || exit /b !ERRORLEVEL!
 popd
 
-"%PYTHON%" setup.py install || exit /b
+"%PYTHON%" setup.py install || exit /b !ERRORLEVEL!
 popd
