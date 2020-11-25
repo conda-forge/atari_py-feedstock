6c9cb732a774dbdee62b09b2239265e4d6455875
Vasily Litvinov <vasilij.n.litvinov@intel.com>
2020-11-25 16:00:50 +0300

    Download and unpack zlib sources via conda
    Signed-off-by: Vasily Litvinov <vasilij.n.litvinov@intel.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,10 +3,6 @@ SETLOCAL EnableDelayedExpansion
 pushd "%SRC_DIR%" || exit /b !ERRORLEVEL!
 
 rem Inline "installzlib.bat" and modify so it can be executed without MSYS
-curl -O https://zlib.net/zlib1211.zip || exit /b !ERRORLEVEL!
-mkdir atari_py\ale_interface\src\zlib || exit /b !ERRORLEVEL!
-unzip -o zlib1211.zip || exit /b !ERRORLEVEL!
-xcopy /E zlib-1.2.11 atari_py\ale_interface\src\zlib || exit /b !ERRORLEVEL!
 pushd atari_py\ale_interface\src\zlib || exit /b !ERRORLEVEL!
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release . || exit /b !ERRORLEVEL!
 cmake --build . || exit /b !ERRORLEVEL!
